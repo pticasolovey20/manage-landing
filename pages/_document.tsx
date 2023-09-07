3fdbc799dd35978967b12789f46aba826ea5d382
unknown <dy.yev.solovyov@gmail.com>
2023-09-08 00:45:32 +0300

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+const Document = () => {
+	return (
+		<Html lang="en">
+			<Head />
+			<title>Manage Landing Page</title>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@400;500;600&display=swap"
+				rel="stylesheet"
+			/>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		screens: {
+			xs: "375px",
+			sm: "576px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1200px",
+			xxl: "1440px",
+		},
+
+		extend: {
+			colors: {
+				primary: {
+					"bright-red": "hsl(12, 88%, 59%)",
+					"dark-blue": "hsl(228, 39%, 23%)",
+				},
+
+				neutral: {
+					"dark-grayish Blue": "hsl(227, 12%, 61%)",
+					"very-dark-blue": "hsl(233, 12%, 13%)",
+					"very-pale-red": "hsl(13, 100%, 96%)",
+					"vary-light-gray": "hsl(0, 0%, 98%)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
